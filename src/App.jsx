07f27df2a9dd4d695a4bca225d93c28677300498
EneaSema <sema.enea1993@gmail.com>
2025-06-05 17:02:00 +0200

    resolve-url

--- src/App.jsx
+++ src/App.jsx
@@ -1,48 +1,25 @@
-import axios from "axios";
-import { useEffect, useState } from "react";
-
 function App() {
-  /* DATI PER CHIAMATE AXIOS */
-  const urlApi = "https://api.themoviedb.org/3";
-  const apiKey = "api_key=ecc2f9c18e89d6d5b71e3d9673461c45";
-  // const language = "language=it";
-  const query = "query=scrubs";
+  const appName = import.meta.env.VITE_NAME_APP;
+  console.log(import.meta.env);
+  const urlApi = import.meta.env.VITE_URL_API;
+  const keyApi = import.meta.env.VITE_API_KEY;
+  const query = "Harry";
 
-  /* VARIABILE DI STATO */
-  const [moviesList, setMoviesList] = useState([]);
-
-  /* FUNZIONE ONCHANGE */
-  const handleChangeSearch = (e) => {
-    useEffect(() => {
-      [...moviesList];
-      e.target.name = e.target.value;
-    }, []);
-  };
-
-  /* FUNZIONE ONCLICK BUTTON */
-  const handleClicKButton = (e) => {
-    e.preventDefault();
-    useEffect(() => {
-      axios.get(``).then((res) => {
-        console.log(res.data);
-      });
-      setMoviesList(moviesList);
-    }, []);
-    return e.target.value;
-  };
+  const urlSearchMovies = "{`${urlApi}?${keyApi}`}";
   return (
     <>
-      <h1> BoolFlix</h1>
-      <header>
+      <h1>{appName}</h1>
+
+      {/* <header>
         <div>
-          <input
+          <input value={moviesList}
             onChange={handleChangeSearch}
             type="search"
             placeholder="search"
           />
-          <button onClick={handleClicKButton}>Cerca</button>
+          <button name={moviesList} onClick={handleClicKButton}>Cerca</button>
         </div>
-      </header>
+      </header> */}
     </>
   );
 }
