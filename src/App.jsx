ad263945c140ef12d8120a2de51decb4f72090ea
EneaSema <sema.enea1993@gmail.com>
2025-06-05 13:19:43 +0200

    create-structure-searchbar

--- src/App.jsx
+++ src/App.jsx
@@ -1,11 +1,46 @@
+import axios from "axios";
+import { useEffect, useState } from "react";
+
 function App() {
+  /* DATI PER CHIAMATE AXIOS */
+  const urlApi = "https://api.themoviedb.org/3";
+  const apiKey = "api_key=ecc2f9c18e89d6d5b71e3d9673461c45";
+  // const language = "language=it";
+  const query = "query=scrubs";
+
+  /* VARIABILE DI STATO */
+  const [moviesList, setMoviesList] = useState([]);
+
+  /* FUNZIONE ONCHANGE */
+  const handleChangeSearch = (e) => {
+    useEffect(() => {
+      [...moviesList];
+      e.target.name = e.target.value;
+    }, []);
+  };
+
+  /* FUNZIONE ONCLICK BUTTON */
+  const handleClicKButton = (e) => {
+    e.preventDefault();
+    useEffect(() => {
+      axios.get(``).then((res) => {
+        console.log(res.data);
+      });
+      setMoviesList(moviesList);
+    }, []);
+    return e.target.value;
+  };
   return (
     <>
       <h1> BoolFlix</h1>
       <header>
         <div>
-          <input type="search" placeholder="search" />
-          <button>Cerca</button>
+          <input
+            onChange={handleChangeSearch}
+            type="search"
+            placeholder="search"
+          />
+          <button onClick={handleClicKButton}>Cerca</button>
         </div>
       </header>
     </>
